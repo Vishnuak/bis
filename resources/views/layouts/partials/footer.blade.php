f2e99a3ce226f24dfabc5ba761e45b3b8375e119
arungopi <garunalways@disroot.org>
2019-09-03 11:58:04 +0530

    Footer changed

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center fixed-bottom mb-2">
-    The source code of this project is available on <a href="https://github.com/vishnuak/bis" target="_blank">github</a>.
+   This is an open source project.
 </footer>
